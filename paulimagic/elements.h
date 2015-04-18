183faa9810587e11a89da84def2c7ba25c13ceb3
McUrbn <urban@aadr.de>
2015-04-18 23:10:05 +0200

    +h

---
+++ paulimagic/elements.h
@@ -0,0 +1,17 @@
+//
+//  elements.h
+//  paulimagic
+//
+//  Created by Seifert on 18.04.15.
+//  Copyright (c) 2015 Urban Seifert. All rights reserved.
+//
+
+#ifndef paulimagic_elements_h
+#define paulimagic_elements_h
+
+#include "tensorproduct.h"
+#include <iostream>
+
+auto testFunc() -> void;
+
+#endif
